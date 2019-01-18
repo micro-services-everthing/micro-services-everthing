c32a02f80139cceff4da5de2cdd9295d40a02f4a
bage2014 <893542907@qq.com>
2019-01-18 19:50:09 +0800

    提交

---
+++ micro-services-everthing/everthing-organization/src/main/java/org/everthing/organization/App.java
@@ -0,0 +1,13 @@
+package org.everthing.organization;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
